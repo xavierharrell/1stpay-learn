efc159e17548ca2c0173d6aabeeea7c7fecaee75
Xavier Harrell <xavierharrell@gmail.com>
2014-12-21 23:04:05 -0600

    Added Error catch

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync');
 var sourcemaps = require('gulp-sourcemaps');
+var handleErrors = require('../util/handleErrors');
 var config = require('../config').sass;
 var autoprefixer = require('gulp-autoprefixer');
 
@@ -13,6 +14,7 @@ gulp.task('sass', function(){
 		includePaths: require('node-neat').includePaths
 	}))
 	.pipe(autoprefixer({browsers: ['Last 2 version']}))
+	.on('error', handleErrors)
 	.pipe(gulp.dest(config.dest))
 	.pipe(browserSync.reload({stream:true}));
 });
