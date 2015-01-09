666f3121eea0758297b89beb4894c68a3061da95
Xavier Harrell <xavier@e-mg.com>
2015-01-09 14:04:10 -0600

    Trying out mobile navigation

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
 var test = "test variable";
+
+$(document).ready(function() {
+  var menuToggle = $('#js-mobile-menu').unbind();
+  $('#js-nav-menu').removeClass("show");
+
+  menuToggle.on('click', function(e) {
+    e.preventDefault();
+    $('#js-nav-menu').slideToggle(function(){
+      if($('#js-nav-menu').is(':hidden')) {
+        $('#js-nav-menu').removeAttr('style');
+      }
+    });
+  });
+});
