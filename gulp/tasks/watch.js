e21a39ce910fa002c74551a0787f630f0ad9dbbf
Xavier Harrell <xavierharrell@gmail.com>
2015-01-12 05:19:09 -0600

    Made minor changes to entry listing page

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -1,8 +1,7 @@
 var gulp = require('gulp');
 var config = require('../config');
-var watchify = require('./browserify')
 
-gulp.task('watch', ['watchify', 'browserSync'], function(callback){
+gulp.task('watch', ['setWatch', 'browserSync'], function(callback){
 	gulp.watch(config.sass.src, ['sass']);
 	gulp.watch(config.images.src, ['images']);
 	gulp.watch(config.markup.src, ['markup']);
