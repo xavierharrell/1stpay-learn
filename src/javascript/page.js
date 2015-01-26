1f2f2f4cbc2b1071fb86f37f592a6d2e7322c51e
Xavier Harrell <xavierharrell@gmail.com>
2015-01-26 08:07:19 -0600

    Added more functioniality

--- src/javascript/page.js
+++ src/javascript/page.js
@@ -1,18 +1,19 @@
 // Browserify entry point for the page.js bundle (yay JavaScript!)
 
 var $ = require('jquery');
+window.jQuery = $;
 var _ = require('underscore');
-var TYP = require('typeahead');
+var typeahead = require('typeahead');
 // global.js already contains jQuery, so in our config.js file, we
 // are exposing it to other files like this one in the `require` array.
 // Also in config.js, jquery is listed in `external` array for this bundle.
 // This combination lets this file use the jquery module bundled with
 // global.js, instead including it twice!
 
-$(document).ready(function() {
+$(function () {
+
   var menuToggle = $('#js-centered-navigation-mobile-menu').unbind();
   $('#js-centered-navigation-menu').removeClass("show");
-  
   menuToggle.on('click', function(e) {
     e.preventDefault();
     $('#js-centered-navigation-menu').slideToggle(function(){
@@ -21,8 +22,60 @@ $(document).ready(function() {
       }
     });
   });
+
+var substringMatcher = function(strs) {
+  return function findMatches(q, cb) {
+    var matches, substrRegex;
+
+    // an array that will be populated with substring matches
+    matches = [];
+
+    // regex used to determine if a string contains the substring `q`
+    substrRegex = new RegExp(q, 'i');
+
+    // iterate through the pool of strings and for any string that
+    // contains the substring `q`, add it to the `matches` array
+    $.each(strs, function(i, str) {
+      if (substrRegex.test(str)) {
+        // the typeahead jQuery plugin expects suggestions to a
+        // JavaScript object, refer to typeahead docs for more info
+        matches.push({ value: str });
+      }
+    });
+
+    cb(matches);
+  };
+};
+
+var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+  'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+  'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+  'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+  'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+  'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+  'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+  'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+  'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+];
+
+console.log(states);
+
+$('#the-basics .typeahead').typeahead({
+  hint: true,
+  highlight: true,
+  minLength: 1
+},
+{
+  name: 'states',
+  displayKey: 'value',
+  source: substringMatcher(states)
 });
 
-var basic = "hello";
 
-console.log(basic);
+});
+
+
+// var basic = "hello";
+
+// console.log(basic);
+
